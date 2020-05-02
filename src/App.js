fb611fb8e313a61d59aa95e45564b0a79262fabb
Renato Koyama <renato.koyama@gmail.com>
2020-05-02 00:49:11 -0300

    Collection Preview component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 import { Route, Switch } from 'react-router-dom';
-
-const HatsPage =  () => {
-  return (
-    <h1> THIS IS HATS PAGE</h1>
-  )
-}
 
 function App() {
   return (
     <div> 
       <Switch> 
         <Route exact path="/" component ={HomePage}></Route>
-        <Route path="/hats" component ={HatsPage}></Route>
+        <Route path="/shop" component ={ShopPage}></Route>
       </Switch>
     </div>
   )
