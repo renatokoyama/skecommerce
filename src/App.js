6d90e89fedb64f8f62797c30e2402e2ec67999cc
Renato Koyama <renato.koyama@gmail.com>
2020-05-02 14:29:54 -0300

    Sign-in component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
+
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
-import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div> 
+    <div>
       <Header />
-      <Switch> 
-        <Route exact path="/" component ={HomePage}></Route>
-        <Route path="/shop" component ={ShopPage}></Route>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
       </Switch>
     </div>
-  )
+  );
 }
 
 export default App;
