abd3382258a42b86672d2086d0354434f1d8a958
Renato Koyama <renato.koyama@gmail.com>
2020-05-02 18:33:02 -0300

    Fix signin styles

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -19,7 +19,7 @@ export default function Header({currentUser}) {
         </Link>
         {
           currentUser ? 
-          <div className="option" onClick={auth.signOut}>SIGN OUT</div>
+          <div className="option" onClick={()=> {auth.signOut()} }>SIGN OUT</div>
           :
           <Link className="option" to="/signin">SIGN IN</Link>
         }
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -51,8 +51,10 @@ class SignIn extends React.Component {
             label='password'
             required
           />
-          <CustomButton type='submit'> SIGN IN </CustomButton>
-          <CustomButton onClick={signInWithGoogle}> SIGN IN WITH GOOGLE</CustomButton>
+          <div className="buttons">
+            <CustomButton type='submit'> SIGN IN </CustomButton>
+            <CustomButton onClick={signInWithGoogle}> SIGN IN WITH GOOGLE</CustomButton>
+          </div>
         </form>
       </div>
     );
