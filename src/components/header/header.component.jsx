e050bcf1988519c072d5b55c0d9837d06ca2e68b
Renato Koyama <renato.koyama@gmail.com>
2020-05-02 18:26:59 -0300

    Google oauth

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./header.style.scss";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
+import {auth} from '../../firebase/firebase.utils';
 
-export default function Header() {
+export default function Header({currentUser}) {
   return (
     <div className="header">
       <Link className="logo-container" to="/">
@@ -16,6 +17,12 @@ export default function Header() {
         <Link className="option" to="/contact">
           CONTACT
         </Link>
+        {
+          currentUser ? 
+          <div className="option" onClick={auth.signOut}>SIGN OUT</div>
+          :
+          <Link className="option" to="/signin">SIGN IN</Link>
+        }
       </div>
     </div>
   );
