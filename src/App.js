81cdeced2a59f7499f681c5f5510cc01d107fed0
Renato Koyama <renato.koyama@gmail.com>
2020-05-03 02:25:22 -0300

    redirecting to home when access login page when user is already logged in

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
@@ -10,10 +10,6 @@ import { setCurrentUser } from './redux/user/user.action';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
-  constructor() {
-    super();
-  }
-
   unsubscribeFromAuth = null;
 
   componentDidMount() {
@@ -48,15 +44,18 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
-          <Route path='/signin' component={SignInAndSignUpPage} />
+          <Route exact path='/signin' render={() => this.props.currentUser? (<Redirect to='/' />): (<SignInAndSignUpPage/>)} />
         </Switch>
       </div>
     );
   }
 }
 
+const mapStateToProps = ({user})=>({ 
+  currentUser: user
+});
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
